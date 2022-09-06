e0998ca0ccaa60174145b278390601dfac129c97
Hilmi Ilyas Rizaldi <developer.hilmi@gmail.com>
2022-09-06 21:58:52 +0700

    Remove unused parameter

--- flipperandroidnoop/src/main/java/com/facebook/flipper/plugins/network/FlipperOkhttpInterceptor.kt
+++ flipperandroidnoop/src/main/java/com/facebook/flipper/plugins/network/FlipperOkhttpInterceptor.kt
@@ -4,8 +4,7 @@ import okhttp3.Interceptor
 import okhttp3.Response
 
 class FlipperOkhttpInterceptor(
-    private val plugin: NetworkFlipperPlugin,
-    private val isMockResponseSupported: Boolean? = true
+    private val plugin: NetworkFlipperPlugin
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         return chain.proceed(chain.request())
